8e4fb1b3dcae7cf1872f5872035d6c1f7eeed43d
Krian <2793260947@qq.com>
2022-12-31 15:41:24 +0800

    初始化仓库提交 — 2022/12/31

---
+++ src/test/java/wiki/csbox/webstack/WebStackApplicationTests.java
@@ -0,0 +1,13 @@
+package wiki.csbox.webstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
